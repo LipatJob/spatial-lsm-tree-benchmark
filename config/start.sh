9a8970f123bfc8291c23abb914c9259196f2aff2
Job Lipat <job.lipat@maya.ph>
2025-04-28 15:23:15 +0800

    Enhance AsterixDB startup script and Docker configuration
    - Updated the start.sh script to include installation of 'procps' and added a sleep period to ensure services are properly initialized.
    - Modified the logging mechanism to capture disk I/O metrics for the AsterixDB cluster, creating a dedicated log directory for disk metrics.
    - Updated docker-compose.yaml to mount a new volume for disk logs, ensuring that disk I/O metrics are stored persistently.

--- config/start.sh
+++ config/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+apt-get update
+apt-get install -y procps
+
 CONFIGDIR="/opt/asterixdb/config"
 BINDIR="/opt/asterixdb/bin"
 LOGSDIR="/opt/asterixdb/logs"
@@ -21,10 +24,24 @@ UNIX_TIMESTAMP=$(date +%s)
 
 echo 
 echo "INFO: Starting AsterixDB cluster..."
-"$BINDIR/asterixncservice" -logdir - -config-file "$CONFIGDIR/nc.conf" >> "$LOGSDIR/nc.log" 2>&1 &
-"$BINDIR/asterixcc" -config-file "$CONFIGDIR/cc.conf" >> "$LOGSDIR/cc.log" 2>&1 &
-"$BINDIR/asterixhelper" wait_for_cluster -timeout 90 &
+"$BINDIR/asterixncservice" -logdir - -config-file "$CONFIGDIR/nc.conf" >> "$LOGSDIR/nc.log" &
+"$BINDIR/asterixcc" -config-file "$CONFIGDIR/cc.conf" >> "$LOGSDIR/cc.log" &
+"$BINDIR/asterixhelper" wait_for_cluster -timeout 90
+
 iotop -Pabotqk > $LOGSDIR/network/iotop_$UNIX_TIMESTAMP.log &
-echo "I/O metrics are being written to $LOGSDIR/network/iotop.log" &
+echo "I/O metrics are being written to $LOGSDIR/network/iotop.log"
+
+sleep 5
+NCPID=$(ps aux | grep "[a]sterixcc" | awk '{print $2}')
+echo 
+mkdir "$LOGSDIR/disk"
+while true; do
+  echo "---"
+  echo "Time: $(date "+%Y-%m-%d %H:%M:%S")"
+  cat "/proc/$NCPID/io"
+  sleep 1
+done >> "$LOGSDIR/disk/disk_cc_$UNIX_TIMESTAMP.log" &
+
+
 echo "INFO: AsterixDB cluster started. Logs are being written to $LOGSDIR" &
 tail -f "$LOGSDIR/nc.log" "$LOGSDIR/nc-asterix_nc1.log";
