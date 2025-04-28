63ac27806761268e640953725848a31e33f75ba8
Job Lipat <job.lipat@maya.ph>
2025-04-28 14:48:13 +0800

    Refactor CREATE_DATABASE.sql to correct dataset and index definitions for BTree

--- CREATE_DATABASE.sql
+++ CREATE_DATABASE.sql
@@ -19,6 +19,7 @@ WITH {
     }
 };
 CREATE INDEX Location_RTree_Constant_Index on Locations_RTree_Constant(location) TYPE rtree;
+
 CREATE DATASET Locations_RTree_Concurrent(LocationType) PRIMARY KEY id
 WITH {
     "merge-policy": {
@@ -45,8 +46,5 @@ WITH {
 };
 CREATE INDEX Locations_RTree_CorrelatedPrefix_Index on Locations_RTree_CorrelatedPrefix(location) TYPE rtree;
 
-CREATE DATASET Locations_BTree_Constant(LocationType) PRIMARY KEY id;
-CREATE INDEX Locations_BTree_Constant_Index on Locations_BTree_Constant(location) TYPE rtree;
-
-CREATE DATASET Locations_BTree_Concurrent(LocationType) PRIMARY KEY id;
-CREATE INDEX Locations_BTree_Concurrent_Index on Locations_BTree_Concurrent(location) TYPE rtree;
+CREATE DATASET Locations_BTree(LocationType) PRIMARY KEY id;
+CREATE INDEX Locations_BTree_Constant on Locations_BTree(location) TYPE btree;
